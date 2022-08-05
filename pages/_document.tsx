409d9f1730f6ab4ebde933ee1b9cfcdeeff0946c
Daniil <dadyarri@gmail.com>
2022-08-05 18:55:02 +0300

    edit home page

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+// noinspection HtmlRequiredTitleElement
+
+import NextDocument, { Head, Html, Main, NextScript } from "next/document";
+import { ColorModeScript } from "@chakra-ui/react";
+import React from "react";
+
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html lang={"ru"}>
+        <Head />
+        <body>
+          <ColorModeScript initialColorMode={"light"} />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
