4454672c93f5128181ea2dcace419ef873d5acee
Daniil <dadyarri@gmail.com>
2022-08-05 18:54:48 +0300

    add navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,27 @@
+import {Box, Button, Container, Flex, Heading} from "@chakra-ui/react";
+import {HiOutlineDownload} from "react-icons/hi";
+
+const Navbar = () => {
+  return (
+    <Box as={"nav"} w={"100%"} zIndex={2}>
+      <Container display={"flex"} p={2} maxW={"container.md"}>
+        <Flex align={"left"} mr={5}>
+          <Heading as={"h1"} size={"lg"} letterSpacing={"tighter"}>
+            dadyarri
+          </Heading>
+        </Flex>
+        {/*@ts-ignore 2322*/}
+        <Box flex={1} align={"right"}>
+          <Button
+            aria-label={"Download CV"}
+            colorScheme={"blue"}
+            rightIcon={<HiOutlineDownload />}
+            color={"white"}
+          >Скачать PDF</Button>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+export default Navbar;
