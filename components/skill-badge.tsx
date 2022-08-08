0aff74cbde2912d2dedbc1b03e18e87e70e6b531
Daniil <dadyarri@gmail.com>
2022-08-08 07:49:57 +0300

    add tooltip for skill badge

--- components/skill-badge.tsx
+++ components/skill-badge.tsx
@@ -1,12 +1,15 @@
-import {Badge} from "@chakra-ui/react";
+import {Badge, Tooltip} from "@chakra-ui/react";
 import React from "react";
 
 interface Props {
-    children: React.ReactNode;
+  technology: string;
+  tooltip: string;
 }
 
-const SkillBadge = ({ children }: Props) => {
-  return <Badge variant={"solid"} colorScheme={"blue"} fontSize={15} mx={3} my={2}>{children}</Badge>
+const SkillBadge = ({ technology, tooltip }: Props) => {
+  return <Tooltip label={tooltip}>
+    <Badge variant={"solid"} colorScheme={"blue"} fontSize={15} mx={3} my={2}>{technology}</Badge>
+  </Tooltip>
 }
 
 export default SkillBadge;
