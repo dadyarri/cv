7d8e174978a248803f132179a9e333896422a41e
Daniil <dadyarri@gmail.com>
2022-09-25 00:27:59 +0300

    fix links

--- components/bio-card.tsx
+++ components/bio-card.tsx
@@ -1,4 +1,5 @@
 import { Badge, Box, Heading, Link, Text } from "@chakra-ui/react";
+import NextLink from "next/link";
 import React from "react";
 
 type Props = {
@@ -8,8 +9,9 @@ type Props = {
   link?: string;
 };
 
-export const BioCard = ({ title, year, desc, link }: Props) => (
-  <Box p={5} shadow={"sm"} borderWidth={"1px"} m={3}>
+export const BioCard = ({ title, year, desc, link }: Props) => {
+  console.log(link);
+  return <Box p={5} shadow={"sm"} borderWidth={"1px"} m={3}>
     <Heading size={"md"}>
       {title} <Badge colorScheme={"blue"}>{year}</Badge>
     </Heading>
@@ -19,9 +21,10 @@ export const BioCard = ({ title, year, desc, link }: Props) => (
         <br />
         <Text>
           {/*@ts-ignore*/}
-          Подробнее: <Link href={{ link }}>{link}</Link>
+          Подробнее: <NextLink href={link} passHref>
+          <Link>{link}</Link></NextLink>
         </Text>
       </>
     ) : null}
   </Box>
-);
+};
