491c900dbf0414cda9d7774870e674ee95c21f86
Daniil <dadyarri@gmail.com>
2022-12-09 15:16:42 +0300

    add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,17 @@ import type { NextPage } from "next";
 import Navbar from "../components/navbar";
 import Resume from "../components/resume";
 import { Container, Hide } from "@chakra-ui/react";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
+      <><Head><title>Даниил Голубев | Резюме</title></Head>
     <Container maxW={"120ch"}>
       <Hide breakpoint={"print"}>
         <Navbar />
       </Hide>
       <Resume />
-    </Container>
+    </Container></>
   );
 };
 
